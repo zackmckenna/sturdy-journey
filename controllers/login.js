e2c9ab79e9a74fe6cba6288e1cf9fa7d8e8f0ff8
Zack McKenna <zackmckenna@gmail.com>
2019-12-09 16:40:55 -0500

    added console.log to check login

--- controllers/login.js
+++ controllers/login.js
@@ -5,28 +5,33 @@ const User = require('../models/user');
 
 loginRouter.post('/', async (request, response) => {
   const body = request.body;
+  console.log(request.body);
 
-  const user = await User.findOne({ username: body.username });
-  const passwordCorrect = user === null
-    ? false
-    : await bcrypt.compare(body.password, user.passwordHash);
+  try {
+    const user = await User.findOne({ username: body.username });
+    const passwordCorrect = user === null
+      ? false
+      : await bcrypt.compare(body.password, user.passwordHash);
 
-  if (!(user && passwordCorrect)) {
-    return response.status(401).json({
-      error: 'invalid username or password'
-    });
+    if (!(user && passwordCorrect)) {
+      return response.status(401).json({
+        error: 'invalid username or password'
+      });
+    }
+
+    const userForToken = {
+      username: user.username,
+      id: user._id,
+    };
+
+    const token = jwt.sign(userForToken, process.env.SECRET);
+
+    response
+      .status(200)
+      .send({ token, username: user.username, name: user.name });
+  } catch (exception) {
+    console.log(exception);
   }
-
-  const userForToken = {
-    username: user.username,
-    id: user._id,
-  };
-
-  const token = jwt.sign(userForToken, process.env.SECRET);
-
-  response
-    .status(200)
-    .send({ token, username: user.username, name: user.name });
 });
 
 module.exports = loginRouter;
